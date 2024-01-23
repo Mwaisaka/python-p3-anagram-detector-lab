df1d5e0912806fc2ff453d46ee79e48fda171e09
Frank <frankkashere@gmail.com>
2024-01-23 19:18:14 +0300

    Completed code

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+    
+    def match(self, possible_matches):
+        word_sorted=sorted(self.word)
+        return [anagram for anagram in possible_matches if sorted(anagram.lower())==word_sorted]
+ 
+ 
